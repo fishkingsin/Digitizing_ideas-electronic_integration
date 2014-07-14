6d105f389031e4efa56bb8560a09f0bb635bbc8b
Kong king sin <fishkingsin@gmail.com>
2014-07-14 16:14:52 +0800

    update all

--- Arduino/SCM_OWeek_LEDStrip/SCM_OWeek_LEDStrip.ino
+++ Arduino/SCM_OWeek_LEDStrip/SCM_OWeek_LEDStrip.ino
@@ -1,38 +1,115 @@
+/*
+  Serial RGB controller
+ 
+ Reads a serial input string looking for three comma-separated
+ integers with a newline at the end. Values should be between 
+ 0 and 255. The sketch uses those values to set the color 
+ of an RGB LED attached to pins 9 - 11.
+ 
+ The circuit:
+ * Common-anode RGB LED cathodes attached to pins 9 - 11
+ * LED anode connected to pin 13
+ 
+ To turn on any given channel, set the pin LOW.  
+ To turn off, set the pin HIGH. The higher the analogWrite level,
+ the lower the brightness.
+ 
+ created 29 Nov 2010
+ by Tom Igoe
+ 
+ This example code is in the public domain. 
+ */
+//https://github.com/iKenndac/Arduino-Dioder-Playground
 #include "RGBdriver.h"
 #define CLK 2//pins definitions for the driver        
 #define DIO 3
 RGBdriver Driver(CLK,DIO);
-#define NUM_BYTES   4
-char inputBuffer[NUM_BYTES];
-unsigned char r = 0;
-unsigned char g = 0;
-unsigned char b = 0;
-void setup()  
-{ 
-  Serial.begin(112500);
-}  
-void loop()  
-{
-  while (Serial.available() ==0 ) {
-    Serial.readBytes(inputBuffer, NUM_BYTES);
+// Protocol details (two header bytes, 12 value bytes, checksum)
 
-    int head = inputBuffer[0];
+const int kProtocolHeaderFirstByte = 0xBA;
+const int kProtocolHeaderSecondByte = 0xBE;
 
-    if(head==0)
-    {
-      r = (unsigned char)(inputBuffer[1]);
-      g = (unsigned char)(inputBuffer[2]);
-      b = (unsigned char)(inputBuffer[3]);
-    }
+const int kProtocolHeaderLength = 2;
+const int kProtocolBodyLength = 12;
+const int kProtocolChecksumLength = 1;
 
+// Buffers and state
 
-    Driver.begin(); // begin
-    Driver.SetColor(r,g,b); //Blue. first node data
-    Driver.end();
+bool appearToHaveValidMessage;
+byte receivedMessage[12];
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
   }
 
+}
+
+void loop() {
+  int availableBytes = Serial.available();
+  
+  if (!appearToHaveValidMessage) {
+    
+    // If we haven't found a header yet, look for one.
+    if (availableBytes >= kProtocolHeaderLength) {
+      
+      // Read then peek in case we're only one byte away from the header.
+      byte firstByte = Serial.read();
+      byte secondByte = Serial.peek();
+      
+      if (firstByte == kProtocolHeaderFirstByte &&
+          secondByte == kProtocolHeaderSecondByte) {
+            
+          // We have a valid header. We might have a valid message!
+          appearToHaveValidMessage = true;
+          
+          // Read the second header byte out of the buffer and refresh the buffer count.
+          Serial.read();
+          availableBytes = Serial.available();
+      }
+    }
+  }
+  
+  if (availableBytes >= (kProtocolBodyLength + kProtocolChecksumLength) && appearToHaveValidMessage) {
+     
+    // Read in the body, calculating the checksum as we go.
+    byte calculatedChecksum = 0;
+    
+    for (int i = 0; i < kProtocolBodyLength; i++) {
+      receivedMessage[i] = Serial.read();
+      calculatedChecksum ^= receivedMessage[i];
+    }
+    
+    byte receivedChecksum = Serial.read();
+    
+    if (receivedChecksum == calculatedChecksum) {
+      // Hooray! Push the values to the output pins.
+      
+      Driver.begin();
+      Driver.SetColor(receivedMessage[0], receivedMessage[1] , receivedMessage[2]);
+      Driver.SetColor(receivedMessage[3], receivedMessage[4], receivedMessage[5]);
+      Driver.end();
+      
+//      Serial.print("OK");
+//      Serial.write(byte(10));
+      
+    } else {
+      
+//      Serial.print("FAIL");
+//      Serial.write(byte(10));
+    }
+    
+    appearToHaveValidMessage = false;
+  }
 
 }
+
+
+
+
+
 
 
 
