8dbc195a66f709b22c664996bece3ee5974af778
Kong king sin <fishkingsin@gmail.com>
2014-08-20 12:27:06 +0800

    added New Arduino project
     for newer version led strip driver

---
+++ Arduino/SCM_OWeek_LEDStrip2/SCM_OWeek_LEDStrip2.ino
@@ -0,0 +1,129 @@
+/*
+  Serial RGB controller
+ 
+ Reads a serial input string looking for three comma-separated
+ integers with a newline at the end. Values should be between 
+ 0 and 255. The sketch uses those values to set the color 
+ of an RGB LED attached to pins 9 - 11.
+ 
+ The circuit:
+ * Common-anode RGB LED cathodes attached to pins 9 - 11
+ * LED anode connected to pin 13
+ 
+ To turn on any given channel, set the pin LOW.  
+ To turn off, set the pin HIGH. The higher the analogWrite level,
+ the lower the brightness.
+ 
+ created 29 Nov 2010
+ by Tom Igoe
+ 
+ This example code is in the public domain. 
+ */
+//https://github.com/iKenndac/Arduino-Dioder-Playground
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include "Suli.h"
+#include "LED_Strip_Arduino.h"
+
+#define CLK 2//pins definitions for the driver        
+#define DIO 3
+led_strip Driver(CLK,DIO);
+// Protocol details (two header bytes, 12 value bytes, checksum)
+
+const int kProtocolHeaderFirstByte = 0xBA;
+const int kProtocolHeaderSecondByte = 0xBE;
+
+const int kProtocolHeaderLength = 2;
+const int kProtocolBodyLength = 12;
+const int kProtocolChecksumLength = 1;
+
+// Buffers and state
+
+bool appearToHaveValidMessage;
+byte receivedMessage[kProtocolBodyLength];
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+}
+
+void loop() {
+  int availableBytes = Serial.available();
+  
+  if (!appearToHaveValidMessage) {
+    
+    // If we haven't found a header yet, look for one.
+    if (availableBytes >= kProtocolHeaderLength) {
+      
+      // Read then peek in case we're only one byte away from the header.
+      byte firstByte = Serial.read();
+      byte secondByte = Serial.peek();
+      
+      if (firstByte == kProtocolHeaderFirstByte &&
+          secondByte == kProtocolHeaderSecondByte) {
+            
+          // We have a valid header. We might have a valid message!
+          appearToHaveValidMessage = true;
+          
+          // Read the second header byte out of the buffer and refresh the buffer count.
+          Serial.read();
+          availableBytes = Serial.available();
+      }
+    }
+    else
+    {
+       Serial.println("FAIL");
+    }
+  }
+  
+  if (availableBytes >= (kProtocolBodyLength + kProtocolChecksumLength) && appearToHaveValidMessage) {
+     
+    // Read in the body, calculating the checksum as we go.
+    byte calculatedChecksum = 0;
+    
+    for (int i = 0; i < kProtocolBodyLength; i++) {
+      receivedMessage[i] = Serial.read();
+      calculatedChecksum ^= receivedMessage[i];
+    }
+    
+    byte receivedChecksum = Serial.read();
+    
+    if (receivedChecksum == calculatedChecksum) {
+      // Hooray! Push the values to the output pins.
+      
+      Driver.begin();
+      
+      for(int i = 0 ; i < 4 ; i++)
+      {
+        int index = i*3;
+        Driver.setColor(receivedMessage[index], receivedMessage[index+1] , receivedMessage[index+2]);
+      }
+      Driver.end();
+      
+      Serial.println("OK");
+      
+    } else {
+      
+      Serial.println("FAIL");
+    }
+    
+    appearToHaveValidMessage = false;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
